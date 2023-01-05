642daed337e252e591a58e472932496ff4ce497e
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-05 14:43:17 +0700

    update extends base service for question service

--- src/apis/questions/questions.service.ts
+++ src/apis/questions/questions.service.ts
@@ -1,26 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import PROVIDE_NAME from 'src/common/provide-name';
+import { BaseService } from '../bases/base.service';
+import { Question } from '../entities.index';
+import { CreateRoleDto } from '../roles/dto/create-role.dto';
+import { UpdateRoleDto } from '../roles/dto/update-role.dto';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
-export class QuestionsService {
-  create(createQuestionDto: CreateQuestionDto) {
-    return 'This action adds a new question';
-  }
-
-  findAll() {
-    return `This action returns all questions`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} question`;
-  }
-
-  update(id: number, updateQuestionDto: UpdateQuestionDto) {
-    return `This action updates a #${id} question`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} question`;
+export class QuestionsService extends BaseService<
+  CreateQuestionDto,
+  UpdateQuestionDto,
+  number
+> {
+  constructor(
+    @Inject(PROVIDE_NAME.QUESTION_REPOSITORY)
+    questionRepository: typeof Question,
+  ) {
+    super(questionRepository);
   }
 }
