bc97e733e7877ebb231232328768d86a709e894b
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-05 14:10:55 +0700

    remove field in update dtos 'cause it is unnecessary

--- src/apis/roles/dto/update-role.dto.ts
+++ src/apis/roles/dto/update-role.dto.ts
@@ -1,13 +1,4 @@
-import { IsNumber, IsOptional, IsString } from '@nestjs/class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {
-  @IsNumber()
-  @IsOptional()
-  id?: number;
-
-  @IsString()
-  @IsOptional()
-  name?: string;
-}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
--- src/apis/tags/dto/update-tag.dto.ts
+++ src/apis/tags/dto/update-tag.dto.ts
@@ -1,17 +1,4 @@
-import { IsNumber, IsOptional, IsString } from '@nestjs/class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTagDto } from './create-tag.dto';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {
-  @IsNumber()
-  @IsOptional()
-  id?: number;
-
-  @IsNumber()
-  @IsOptional()
-  parentId?: number;
-
-  @IsString()
-  @IsOptional()
-  title?: string;
-}
+export class UpdateTagDto extends PartialType(CreateTagDto) {}
