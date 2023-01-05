ec8cb3c88420a9ab7968d81e763bd818f1cc0624
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-05 14:36:29 +0700

    addtional question provider

--- src/apis/questions/questions.module.ts
+++ src/apis/questions/questions.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import PROVIDE_NAME from 'src/common/provide-name';
+import { Question } from '../entities.index';
 
 @Module({
   controllers: [QuestionsController],
-  providers: [QuestionsService]
+  providers: [
+    QuestionsService,
+    { provide: PROVIDE_NAME.QUESTION_REPOSITORY, useValue: Question },
+  ],
 })
 export class QuestionsModule {}
