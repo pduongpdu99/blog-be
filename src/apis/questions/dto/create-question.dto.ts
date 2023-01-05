018509dbc80bc6298e52e562fdb68978faa6069a
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-05 14:42:53 +0700

    addtional fields into create question dto

--- src/apis/questions/dto/create-question.dto.ts
+++ src/apis/questions/dto/create-question.dto.ts
@@ -1,1 +1,16 @@
-export class CreateQuestionDto {}
+import { IsNumber, IsOptional, IsString } from "@nestjs/class-validator";
+
+export class CreateQuestionDto {
+  @IsNumber()
+  @IsOptional()
+  id?: number;
+
+  @IsString()
+  userId: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  describe: string;
+}
