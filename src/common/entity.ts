f2a56f91a0bb983069cf97874b5b214503e900df
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-07 21:01:01 +0700

    update attribute at create update delete date column

--- src/common/entity.ts
+++ src/common/entity.ts
@@ -1,16 +1,16 @@
-import { Column, Model } from 'sequelize-typescript';
+import { Column, DataType, Model } from 'sequelize-typescript';
 export class BaseEntity extends Model {
-  @Column
+  @Column({ type: DataType.BIGINT })
   createdDate: number = new Date().getTime();
 
-  @Column
+  @Column({ type: DataType.BIGINT })
   updatedDate: number = new Date().getTime();
 
-  @Column
+  @Column({ type: DataType.BIGINT })
   deletedDate: number | null = null;
 
   @Column
-  createdBy: string;
+  createdBy: string | null;
 
   @Column
   updatedBy: string | null;
