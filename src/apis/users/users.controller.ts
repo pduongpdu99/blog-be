ae08162bb7a22c5ce8f63e7b4a32bf59f975fc0e
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-07 22:12:53 +0700

    update field need to find value template

--- src/apis/users/users.controller.ts
+++ src/apis/users/users.controller.ts
@@ -17,7 +17,9 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    return this.usersService.create(createUserDto, {
+      email: createUserDto.email,
+    });
   }
 
   @Get()
