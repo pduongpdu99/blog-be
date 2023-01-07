a2abc7bb4be587a798eea7c06a21ce991067ad83
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-07 22:13:26 +0700

    override create feature into user service

--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
 import PROVIDE_NAME from 'src/common/provide-name';
+import { HttpResponse } from '../bases/base.exception';
 import { BaseService } from '../bases/base.service';
 import { User } from '../entities.index';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UsersService extends BaseService<
   CreateUserDto,
@@ -15,5 +17,29 @@ export class UsersService extends BaseService<
     @Inject(PROVIDE_NAME.USER_REPOSITORY) private userRepository: typeof User,
   ) {
     super(userRepository);
+  }
+
+  /**
+   * create data based on dto
+   * @param dto
+   * @returns
+   */
+  async create(dto: CreateUserDto, fieldForCheckExists?: any) {
+    const data = await this.repository.findOne({
+      where: { ...fieldForCheckExists },
+    });
+    if (data)
+      throw new HttpResponse(
+        'Cannot create record when it exist',
+        HttpStatus.CONFLICT,
+      );
+
+    if (!dto.id) dto.id = uuidv4();
+
+    return new HttpResponse(
+      'Getting all record successfully',
+      HttpStatus.CREATED,
+      await this.repository.create(dto),
+    );
   }
 }
