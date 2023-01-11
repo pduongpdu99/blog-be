40387bc25ab92c410d5b5698eba1c7bb6416f3be
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-12 02:09:32 +0700

    remove try catch at base service

--- src/apis/bases/base.service.ts
+++ src/apis/bases/base.service.ts
@@ -1,6 +1,5 @@
-import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Op } from 'sequelize';
-import { HttpResponse } from './base.exception';
 
 @Injectable()
 export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
@@ -15,47 +14,39 @@ export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
    * @return
    */
   async create(dto: CreateDtoTemplate, fieldForCheckExists?: any) {
-    try {
-      // validate field FK id
-      if (
-        !(fieldForCheckExists == undefined || fieldForCheckExists == null) &&
-        Object.keys(fieldForCheckExists).length > 0
-      ) {
-        for (const key of Object.keys(fieldForCheckExists)) {
-          const isIdKeyValid = key.match(/^[a-zA-Z0-9]+Id$/g);
-          const isString = typeof fieldForCheckExists[key] === 'string';
-          const isStringValid = fieldForCheckExists[key].match(/^\w+$/g);
-          const isNumber = typeof fieldForCheckExists[key] === 'number';
-          const isNumberValid = fieldForCheckExists[key].match(/^\d+$/g);
-          if (!(isIdKeyValid && isString && isStringValid))
-            throw new HttpResponse(
-              'It is not string invalid',
-              HttpStatus.BAD_REQUEST,
-            );
+    // validate field FK id
+    if (
+      !(fieldForCheckExists == undefined || fieldForCheckExists == null) &&
+      Object.keys(fieldForCheckExists).length > 0
+    ) {
+      for (const key of Object.keys(fieldForCheckExists)) {
+        const isIdKeyValid = key.match(/^[a-zA-Z0-9]+Id$/g);
+        const isString = typeof fieldForCheckExists[key] === 'string';
+        const isStringValid = fieldForCheckExists[key].match(/^\w+$/g);
+        const isNumber = typeof fieldForCheckExists[key] === 'number';
+        const isNumberValid = fieldForCheckExists[key].match(/^\d+$/g);
+        if (!(isIdKeyValid && isString && isStringValid))
+          throw new HttpException(
+            'It is not string invalid',
+            HttpStatus.BAD_REQUEST,
+          );
 
-          if (!(isIdKeyValid && isNumber && isNumberValid))
-            throw new HttpResponse(
-              'It is not number invalid',
-              HttpStatus.BAD_REQUEST,
-            );
-        }
+        if (!(isIdKeyValid && isNumber && isNumberValid))
+          throw new HttpException(
+            'It is not number invalid',
+            HttpStatus.BAD_REQUEST,
+          );
       }
-
-      const data = (await this.findAll(fieldForCheckExists)).data;
-      if (data && data.length > 0)
-        throw new HttpResponse(
-          'Cannot create record when it exist',
-          HttpStatus.CONFLICT,
-        );
-
-      return new HttpResponse(
-        'Create record successfully',
-        HttpStatus.CREATED,
-        data,
-      );
-    } catch (error: any) {
-      return error;
     }
+
+    const data = (await this.findAll(fieldForCheckExists)).data;
+    if (data && data.length > 0)
+      throw new HttpException(
+        'Cannot create record when it exist',
+        HttpStatus.CONFLICT,
+      );
+
+    return data;
   }
 
   /**
@@ -63,23 +54,15 @@ export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
    * @return
    */
   async findAll(queries?: any) {
-    try {
-      let where: any = { deletedDate: { [Op.eq]: null } };
-      if (queries) {
-        const q: any = {};
-        Object.keys(queries).forEach((key) => {
-          q[key] = { [Op.eq]: queries[key] };
-        });
-        where = { ...where, ...q };
-      }
-      return new HttpResponse(
-        'Create record successfully',
-        HttpStatus.CREATED,
-        await this.repository.findAll({ where }),
-      );
-    } catch (error: any) {
-      return error;
+    let where: any = { deletedDate: { [Op.eq]: null } };
+    if (queries) {
+      const q: any = {};
+      Object.keys(queries).forEach((key) => {
+        q[key] = { [Op.eq]: queries[key] };
+      });
+      where = { ...where, ...q };
     }
+    return await this.repository.findAll({ where });
   }
 
   /**
@@ -88,34 +71,24 @@ export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
    * @return
    */
   async findOne(id: T) {
-    try {
-      if (
-        typeof id === 'string' &&
-        !id.match(
-          /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/g,
-        )
-      ) {
-        throw new HttpResponse(
-          'id is not valid string',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      if (typeof id === 'number' && !id.toString().match(/^\d+$/g)) {
-        throw new HttpResponse(
-          'id is not valid number',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      const response = await this.repository.findOne({
-        where: { id },
-      });
-
-      return new HttpResponse('Get user by id', HttpStatus.OK, response);
-    } catch (error: any) {
-      return error;
+    if (
+      typeof id === 'string' &&
+      !id.match(
+        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/g,
+      )
+    ) {
+      throw new HttpException('id is not valid string', HttpStatus.BAD_REQUEST);
     }
+
+    if (typeof id === 'number' && !id.toString().match(/^\d+$/g)) {
+      throw new HttpException('id is not valid number', HttpStatus.BAD_REQUEST);
+    }
+
+    const response = await this.repository.findOne({
+      where: { id },
+    });
+
+    return response;
   }
 
   /**
@@ -125,42 +98,28 @@ export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
    * @return
    */
   async update(id: T, dto: UpdateDtoTemplate) {
-    try {
-      if (
-        typeof id === 'string' &&
-        !id.match(
-          /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/g,
-        )
-      ) {
-        throw new HttpResponse(
-          'id is not valid string',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      if (typeof id === 'number' && !id.toString().match(/^\d+$/g)) {
-        throw new HttpResponse(
-          'id is not valid number',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      const check = await this.repository.findOne({ where: { id } });
-
-      if (!check) {
-        throw new HttpResponse('user is not existed', HttpStatus.BAD_REQUEST);
-      }
-
-      await this.repository.update(dto, { where: { id } });
-
-      return new HttpResponse(
-        'Update user by id successfully',
-        HttpStatus.OK,
-        (await this.findOne(id)).data,
-      );
-    } catch (error: any) {
-      return error;
+    if (
+      typeof id === 'string' &&
+      !id.match(
+        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/g,
+      )
+    ) {
+      throw new HttpException('id is not valid string', HttpStatus.BAD_REQUEST);
     }
+
+    if (typeof id === 'number' && !id.toString().match(/^\d+$/g)) {
+      throw new HttpException('id is not valid number', HttpStatus.BAD_REQUEST);
+    }
+
+    const check = await this.repository.findOne({ where: { id } });
+
+    if (!check) {
+      throw new HttpException('user is not existed', HttpStatus.BAD_REQUEST);
+    }
+
+    await this.repository.update(dto, { where: { id } });
+
+    return this.findOne(id);
   }
 
   /**
@@ -169,17 +128,13 @@ export class BaseService<CreateDtoTemplate, UpdateDtoTemplate, T> {
    * @return
    */
   async remove(id: T) {
-    try {
-      const data = await this.repository.findOne({ where: { id } });
-      if (data == null || data == undefined)
-        throw new HttpResponse(
-          'Cannot remove record when it not exist',
-          HttpStatus.BAD_REQUEST,
-        );
-      await this.repository.destroy({ where: { id } });
-      return new HttpResponse('Delete successfully', HttpStatus.OK);
-    } catch (error: any) {
-      return error;
-    }
+    const data = await this.repository.findOne({ where: { id } });
+    if (data == null || data == undefined)
+      throw new HttpException(
+        'Cannot remove record when it not exist',
+        HttpStatus.BAD_REQUEST,
+      );
+    await this.repository.destroy({ where: { id } });
+    return { message: 'Delete successfully', code: HttpStatus.OK };
   }
 }
