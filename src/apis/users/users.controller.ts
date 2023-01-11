07987e21cf579aaf618664404aea75d8d6d45697
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-11 09:40:09 +0700

    remove catch

--- src/apis/users/users.controller.ts
+++ src/apis/users/users.controller.ts
@@ -9,22 +9,6 @@ export class UsersController {
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    try {
-      const fieldForCheckExists = { email: createUserDto.email };
-      const data = await this.usersService.findAll(fieldForCheckExists);
-      if (data && data.length > 0)
-        throw new HttpResponse(
-          'Cannot create record when it exist',
-          HttpStatus.CONFLICT,
-        );
-
-      return new HttpResponse(
-        'Getting all record successfully',
-        HttpStatus.CREATED,
-        await this.usersService.create(createUserDto),
-      );
-    } catch (error) {
-      return new HttpResponse(error.message, error.code);
-    }
+    return await this.usersService.create(createUserDto);
   }
 }
