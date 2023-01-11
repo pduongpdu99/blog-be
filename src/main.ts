3217a3647ae2703cf8e0d3924a2e1f3f72ed194f
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-12 02:05:34 +0700

    add global filter

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from './apis/bases/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,6 +7,8 @@ async function bootstrap() {
 
   // enable cors
   app.enableCors();
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
