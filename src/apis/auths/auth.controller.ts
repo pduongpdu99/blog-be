e6b5e631d6549bb47ef0d2353f58619645f98dc2
pduongpdu99 <pduongpdu99@gmail.com>
2023-01-12 20:40:47 +0700

    remove test api

--- src/apis/auths/auth.controller.ts
+++ src/apis/auths/auth.controller.ts
@@ -1,20 +1,10 @@
-import {
-  Controller,
-  Post,
-  Body,
-  Get,
-  Headers,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Controller, Post, Body } from '@nestjs/common';
 import { SignupDto } from '../users/dto/sign-up.dto';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   //   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -25,13 +15,5 @@ export class AuthController {
   @Post('auth/signup')
   async signup(@Body() params: SignupDto) {
     return await this.authService.signup(params);
-  }
-
-  @Get('test')
-  async test(@Headers() headers: any) {
-    const accessToken = headers.authorization;
-    return await this.jwtService.verify(accessToken, {
-      publicKey: process.env.PUBLIC_KEY,
-    });
   }
 }
