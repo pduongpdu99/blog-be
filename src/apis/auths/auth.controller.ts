875cfee8859487b74b6081436952121490368753
duongpham <pduongpdu99@gmail.com>
2023-02-18 02:02:08 +0700

    add message log

--- src/apis/auths/auth.controller.ts
+++ src/apis/auths/auth.controller.ts
@@ -1,19 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { SignupDto } from '../users/dto/sign-up.dto';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
+  logger = new Logger();
   constructor(private authService: AuthService) {}
 
   //   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Body() body: any) {
+    this.logger.log('Đăng nhập');
     return await this.authService.login(body);
   }
 
   @Post('auth/signup')
   async signup(@Body() params: SignupDto) {
+    this.logger.log('Đăng ký');
     return await this.authService.signup(params);
   }
 }
