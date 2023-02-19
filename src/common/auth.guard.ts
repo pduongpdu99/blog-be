203527601c0b8fe4b785dbf9f6d1b4169f8968e0
duongpham <pduongpdu99@gmail.com>
2023-02-19 14:52:02 +0700

    remove console log

--- src/common/auth.guard.ts
+++ src/common/auth.guard.ts
@@ -6,6 +6,11 @@ export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    if (!(200 <= response.code && response.code < 300)) {
+      return response.message;
+    }
+
     if (request.headers.authorization) {
       this.jwtService.verify(request.headers.authorization.split(' ')[1], {
         secret: process.env.PRIVATE_KEY,
