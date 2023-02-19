c72bd94ad0ea0627c2f026162c720f93d928c6f6
duongpham <pduongpdu99@gmail.com>
2023-02-19 15:23:38 +0700

    update tag controller, extends BaseController into it

--- src/apis/tags/tags.controller.ts
+++ src/apis/tags/tags.controller.ts
@@ -1,42 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { TagsService } from './tags.service';
-import { CreateTagDto } from './dto/create-tag.dto';
-import { UpdateTagDto } from './dto/update-tag.dto';
+import { BaseController } from '../bases/base.controller';
 
 @Controller('tags')
-export class TagsController {
-  constructor(private readonly tagsService: TagsService) {}
-
-  @Post()
-  create(@Body() createTagDto: CreateTagDto) {
-    return this.tagsService.create(createTagDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.tagsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.tagsService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateTagDto: UpdateTagDto) {
-    return this.tagsService.update(id, updateTagDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.tagsService.remove(id);
+export class TagsController extends BaseController {
+  constructor(private readonly tagsService: TagsService) {
+    super(tagsService);
   }
 }
--- src/apis/tags/tags.module.ts
+++ src/apis/tags/tags.module.ts
@@ -3,11 +3,13 @@ import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 import PROVIDE_NAME from 'src/common/provide-name';
 import { Tag } from '../entities.index';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [TagsController],
   providers: [
     TagsService,
+    JwtService,
     { provide: PROVIDE_NAME.TAG_REPOSITORY, useValue: Tag },
   ],
 })
