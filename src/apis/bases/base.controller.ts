1f6653679ae177b768f579144d36af929e365300
duongpham <pduongpdu99@gmail.com>
2023-02-19 15:27:56 +0700

    update logger

--- src/apis/bases/base.controller.ts
+++ src/apis/bases/base.controller.ts
@@ -13,40 +13,42 @@ import { BaseService } from './base.service';
 
 export class BaseController {
   logger = new Logger();
-  constructor(private readonly baseService: BaseService<any, any, any>) {}
+  constructor(private readonly baseService: BaseService<any, any, any>) {
+    this.logger = new Logger(this.baseService.constructor.name);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
   async create(@Body() createUserDto: any) {
-    this.logger.log(`[${this.baseService.constructor.name}] create`);
+    this.logger.log(`create`);
     return await this.baseService.create(createUserDto, {});
   }
 
   @Get()
   @UseGuards(AuthGuard)
   async findAll() {
-    this.logger.log(`[${this.baseService.constructor.name}] get all`);
+    this.logger.log(`get all`);
     return await this.baseService.findAll();
   }
 
   @Get(':id')
   @UseGuards(AuthGuard)
   async findOne(@Param('id') id: any) {
-    this.logger.log(`[${this.baseService.constructor.name}] find by id`);
+    this.logger.log(`find by id`);
     return await this.baseService.findOne(id);
   }
 
   @Patch(':id')
   @UseGuards(AuthGuard)
   async update(@Param('id') id: any, @Body() updateUserDto: any) {
-    this.logger.log(`[${this.baseService.constructor.name}] update by id`);
+    this.logger.log(`update by id`);
     return await this.baseService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   @UseGuards(AuthGuard)
   async remove(@Param('id') id: any) {
-    this.logger.log(`[${this.baseService.constructor.name}] delete by id`);
+    this.logger.log(`delete by id`);
     return await this.baseService.remove(id);
   }
 }
